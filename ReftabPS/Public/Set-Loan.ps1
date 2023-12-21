7b9f25a11063761f537995f8bc34c2c36c30d290
rvhoyt <rvhoyt@gmail.com>
2023-12-21 15:27:30 -0500

    Create Set-Loan.ps1

---
+++ ReftabPS/Public/Set-Loan.ps1
@@ -0,0 +1,25 @@
+﻿function Set-Loanee() {
+    Param(
+        [parameter(mandatory = $true)]
+        [string]$LoanId,
+      
+        [parameter(mandatory = $true)]
+        [PSCustomObject]$Body,
+      
+        [parameter(mandatory = $true)]
+        [string]$Secret,
+      
+        [parameter(mandatory = $true)]
+        [string]$Public
+    )
+    
+    $Parameters = @{
+        Secret = $secret
+        Public = $public
+        Method = 'PUT'
+        Body = $Body
+        Endpoint = 'loans/' + $LoanId
+    }
+    
+    Invoke-Reftab @Parameters
+}
